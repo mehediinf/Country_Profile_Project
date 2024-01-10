b3203f212b30a7927d5e3966e1bb91a337d8397c
mehedi-hasan <mehediinform@gmail.com>
2024-01-11 00:50:24 +0600

    First Commit Country_Profile_Project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Country_Profile_Project"
+include(":app")
+ 
